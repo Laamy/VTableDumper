ca25858096d63eec24b8b9bb5322496a59d0b0db
yeemi routh <Shade4Keal@gmail.com>
2023-01-01 08:46:20 +1100

    Add project files.

---
+++ VTableDumper/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VTableDumper
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] a1)
+        {
+            string[] lines = Clipboard.GetText().Split('\n');
+
+            StringBuilder sb = new StringBuilder();
+            string curClass = null;
+            foreach (string line in lines)
+            {
+                if (line.Contains("::") && line.Contains(";"))
+                {
+                    string splitLine = line.Split(';')[1];
+                    string[] args = splitLine.Trim().Replace("::", ":").Split(':'); // class, func/args
+
+                    if (curClass == null)
+                    {
+                        curClass = args[0];
+
+                        sb.AppendLine($"class {curClass} {{");
+                        sb.AppendLine("public:");
+                    }
+
+                    sb.AppendLine($"    virtual void {args[1].Replace(" ", "")};");
+                }
+            }
+            sb.AppendLine("}");
+            Console.WriteLine(sb.ToString());
+            Console.ReadLine();
+        }
+    }
+}
