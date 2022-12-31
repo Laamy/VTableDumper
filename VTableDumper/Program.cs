ec39e51a54026bf1f6d18f32bbc019a8970b9419
yeemi routh <Shade4Keal@gmail.com>
2023-01-01 08:56:19 +1100

    arguments are now named

--- VTableDumper/Program.cs
+++ VTableDumper/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
@@ -6,6 +7,16 @@ namespace VTableDumper
 {
     class Program
     {
+        static string[] ParseMethodArguments(string methodCall)
+        {
+            string[] split = methodCall.Split(new char[] { '(', ')', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (split.Length == 1)
+                return null;
+
+            return split.Skip(1).ToArray();
+        }
+
         [STAThread]
         static void Main(string[] a1)
         {
@@ -18,7 +29,7 @@ namespace VTableDumper
                 if (line.Contains("::") && line.Contains(";"))
                 {
                     string splitLine = line.Split(';')[1];
-                    string[] args = splitLine.Trim().Replace("::", ":").Split(':'); // class, func/args
+                    string[] args = splitLine.Trim().Replace("::", ":").Split(':'); // class, func(args)
 
                     if (curClass == null)
                     {
@@ -28,7 +39,20 @@ namespace VTableDumper
                         sb.AppendLine("public:");
                     }
 
-                    sb.AppendLine($"    virtual void {args[1].Replace(" ", "")};");
+                    string[] funcArgs = ParseMethodArguments(args[1].Replace(" ", ""));
+                    string newFuncArgs = "";
+
+                    if (funcArgs.Length > 0)
+                    {
+                        int index = 1;
+                        foreach (string arg in funcArgs)
+                        {
+                            newFuncArgs += arg + " a" + index + ", ";
+                            index++;
+                        }
+                    }
+
+                    sb.AppendLine($"    virtual void {args[1].Split('(')[0]}({newFuncArgs.Trim().Trim(',')});");
                 }
             }
             sb.AppendLine("}");
